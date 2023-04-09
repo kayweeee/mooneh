f11f0de99705f970a7858f3c505e37d31157a10b
Abram Tan <72917689+abramtan@users.noreply.github.com>
2023-04-09 04:49:44 -0400

    get moondust from api

--- my-app/src/Components/GameSideBar.jsx
+++ my-app/src/Components/GameSideBar.jsx
@@ -1,12 +1,35 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, {useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faUser, faHammer } from "@fortawesome/free-solid-svg-icons";
+import axios from 'axios';
+
+const getMoonDust = async () => {
+    const backend_url = 'http://127.0.0.1:3000/accounts'
+    const response = await axios.get(backend_url)
+    .then(res => {
+    const moonDust = res.data[0].moonDust;
+    console.log(moonDust);
+    return moonDust;
+    // refreshPage();
+    });
+
+    return response;
+};
 
 function GameSideBar () {
+
+    const [apiResponse, setApiResponse] = useState("*** now loading ***");
+
+    useEffect(() => {
+        getMoonDust().then(
+            result => setApiResponse(result));
+    },[]);
+
     return (
     <div className='absolute top-0 right-0 w-1/5 grid grid-cols-2 gap-x-12 gap-y-5 me-3 mt-5'>
         <div className="flex rounded-lg border-2 border-grey justify-center items-center gap-2 w-16 m-1">
             <FontAwesomeIcon icon={faMoon} style={{color: "#96a1b6",}} />
-            <p>20</p>
+            <p>{ apiResponse }</p>
         </div>
         <button>
             <div className="flex rounded-full bg-white w-8 h-8 justify-center items-center">
